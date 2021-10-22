c810cbe5d2340fc866c64c4f74bd7fc169c5a98b
Alexandra Paskhaver <apaskhaver@nbp-152-52.nbp.ruw.rutgers.edu>
2021-10-22 09:42:22 -0400

    Practice using different types of quotes to make Strings

---
+++ 02-Welcome-to-Python/objects-strings.py
@@ -0,0 +1,13 @@
+"pizza"
+'pizza'
+"foundingfathers123!"
+"$5.00"
+"You're right"
+'Shakespeare once wrote "To be or not to be" in Hamlet'
+
+"""Triple quotes
+let you make
+multiline
+Strings
+That's pretty
+cool"""
