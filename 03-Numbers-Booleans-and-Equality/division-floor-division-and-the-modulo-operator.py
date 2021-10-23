235fb44521e2a8a4b0c32ef8890002c0eb3a9347
Alexandra Paskhaver <apaskhaver@macbook-pro.myfiosgateway.com>
2021-10-23 16:26:51 -0400

    Practice using mathematical operators

---
+++ 03-Numbers-Booleans-and-Equality/division-floor-division-and-the-modulo-operator.py
@@ -0,0 +1,13 @@
+print(15 / 3)
+print(14 / 3)
+
+print(15 // 3)
+print(14 // 3)
+print(1 // 2)
+print(-1 // 2)
+print(-14 // 3)
+print(-13 // 3)
+
+print(1 % 2)
+print(15 % 3)
+print(10 % 3)
---
+++ 03-Numbers-Booleans-and-Equality/mathematical-expressions.py
@@ -0,0 +1,15 @@
+print(2 + 8)
+print(6.3 + 4.6)
+
+print(3 - 4)
+
+print(3 * 4)
+
+print("Sal" + "ad")
+
+print("Watson" * 3)
+
+print(10 ** 3)
+
+print(2 + 4 * 4)
+print((2 + 4) * 4)
