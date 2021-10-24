b5841b9317b3cdfea1abe041fa1471e35f4dce1e
Alexandra Paskhaver <apaskhaver@macbook-pro.myfiosgateway.com>
2021-10-24 11:05:20 -0400

    Practice assigning variables and making multiple assignments on one line

---
+++ 04-Variables/intro-to-variables.py
@@ -0,0 +1,23 @@
+name = "Alexandra"
+print(name)
+age = 18
+name = "Mary"
+print(name)
+print(age)
+
+favorite_language = "English"
+print(favorite_language)
+print("favorite_language")
+
+print(age + 4)
+print("My name is", name, "and I am", age, "years old.")
+
+fact_or_fiction = 6 < 10
+print(fact_or_fiction)
+
+chameleon = "red"
+print(chameleon)
+chameleon = 123
+print(chameleon)
+chameleon = True
+print(chameleon)
---
+++ 04-Variables/multiple-variable-assignments.py
@@ -0,0 +1,12 @@
+a = 5
+b = 5
+print(a, b)
+
+a = b = 8
+print(a, b)
+
+b = 10
+print(a, b)
+
+a, b, c = 1, 2, 3
+print(a, b, c)
