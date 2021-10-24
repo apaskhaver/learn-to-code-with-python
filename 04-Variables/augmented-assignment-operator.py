ca594e23c1c015f164440346770cccd8591f1b47
Alexandra Paskhaver <apaskhaver@macbook-pro.myfiosgateway.com>
2021-10-24 11:11:42 -0400

    Practice using the augmented assignment operator (+=, -=, *=, /=, %=, et cetera)

---
+++ 04-Variables/augmented-assignment-operator.py
@@ -0,0 +1,31 @@
+a = 1
+a = a + 2
+print(a)
+
+a = 1
+a += 2
+print(a)
+
+word = "pyj"
+word += "ama"
+print(word)
+
+b = 5
+b = b * 3
+print(b)
+
+b = 5
+b *= 3
+print(b)
+
+c = 1
+c -= 1
+print(c)
+
+d = 6
+d /= 5
+print(d)
+
+e = 15
+e %= 5
+print(e)
