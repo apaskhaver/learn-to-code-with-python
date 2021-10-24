b794df7616c623adf3740c642b29ca6dae3cada4
Alexandra Paskhaver <apaskhaver@macbook-pro.myfiosgateway.com>
2021-10-24 10:38:35 -0400

    Practice with types and type conversions

---
+++ 03-Numbers-Booleans-and-Equality/the-type-function.py
@@ -0,0 +1,14 @@
+print(type(5))
+print(type("Hello"))
+print(type(True))
+print(type(4.3))
+
+print(type(5) == type(10))
+
+print(5 == 5.0)
+print(type(5) == type(5.0))
+
+print(type(True) == type(False))
+
+print(type([1, 2, 3]))
+print(type({"Louisiana": "Baton Rouge"}))
---
+++ 03-Numbers-Booleans-and-Equality/type-conversion-with-the-int-float-and-str-functions.py
@@ -0,0 +1,16 @@
+print(int("55"))
+print(int(3.14))
+
+print(float(5))
+print(float("10.32"))
+
+print(str(68084))
+print(str(10.1))
+print(str("sandwich"))
+
+# print(5 + "Hello") # error b/c Python can't concatenate an int and str w/o conversion
+print(str(5) + "Hello")
+
+print(type(3 + 3))
+print(type(3.4 + 6.3))
+print(type(3 + 3.5))
