483c0e2c077e8b343eed0034c4f4eac298cce02f
Alexandra Paskhaver <apaskhaver@macbook-pro.myfiosgateway.com>
2021-10-24 15:16:29 -0400

    Practice the input() function and casting

---
+++ 04-Variables/user-input-with-the-input-function.py
@@ -0,0 +1,13 @@
+# input("Please type in your age: ")
+# print("Successfully inputted.")
+
+# name = input("Please type in your name: ")
+# print("Hello,", name)
+
+# Prompt user for two numbers and display their sum
+# number1 = int(input("Please type the first number: "))
+# number2 = int(input("Please type the second number: "))
+# print("The sum of", number1, "and", number2, "is", number1 + number2)
+
+fahrenheit = float(input("Enter a temperature in Fahrenheit (F) to convert it to a temperature in Celsius (C): "))
+print("The temperature in C is:", 5 / 9 * (fahrenheit - 32))
