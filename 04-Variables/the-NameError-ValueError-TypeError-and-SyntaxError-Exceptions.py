b6436e4f322bb2ac7c3761de8ab076a48886a847
Alexandra Paskhaver <apaskhaver@macbook-pro.myfiosgateway.com>
2021-10-25 08:52:27 -0400

    Practice with 4 common errors: NameError, TypeError, ValueError, SyntaxError

---
+++ 04-Variables/the-NameError-ValueError-TypeError-and-SyntaxError-Exceptions.py
@@ -0,0 +1,24 @@
+# NameErrorException
+# first_name = "Alexandra"
+# print(firstname)
+
+# TypeErrorException
+# print("5" + 3) # Python sees this as an error in concatenating Strings b/c str type is first
+# print(5 + "3") # Python sees this as an error in mathematical addition b/c int type is first
+
+# ValueErrorException
+# int("xyz")
+# int("5.45") # Python can't calculate int b/c String becomes a float
+              # Must convert to float first, as in int(float("5.45")) 
+# print(float(True)) # Not an error, prints 1.0
+# print(float(False)) # Not an error, prints 0.0
+# print(str(True)) # Not an error, prints "True"
+
+# SyntaxErrorException
+# print(
+# int("123)
+
+# float(print)
+
+print(type(5))
+
